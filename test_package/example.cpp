b59f24678c4c1227b9e9303a8ad2d5689189c965
Javier G. Sogo <jgsogo@gmail.com>
2018-03-17 20:54:37 +0100

    customize according to pkg-config

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <apr-1/apu_version.h>
+#include <apu_version.h>
 
 int main(int argc, char* argv[]) {
     std::cout << "apu_version_string: " << apu_version_string() << std::endl;
