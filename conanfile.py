beb4c00e97977c4afd799d816e08dc0cfc8813b5
danimtb <danimanzaneque@gmail.com>
2019-02-19 17:46:26 +0100

    Update requirements

--- conanfile.py
+++ conanfile.py
@@ -13,27 +13,27 @@ class ApacheAPRUtil(ConanFile):
     description = "The mission of the Apache Portable Runtime (APR) project is to create and maintain " \
                   "software libraries that provide a predictable and consistent interface to underlying " \
                   "platform-specific implementations."
-    exports_sources = ["LICENSE", ]
+    exports_sources = ["LICENSE"]
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
+    requires = "apache-apr/1.6.3@jgsogo/stable", "Expat/2.2.5@pix4d/stable"
+    _lib_name = "apr-util-" + version
 
-    lib_name = "apr-util-" + version
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fpic
 
     def configure(self):
         del self.settings.compiler.libcxx  # It is a C library
-
-    def requirements(self):
-        self.requires("apache-apr/1.6.3@jgsogo/stable")
-        self.requires("expat/2.2.5@bincrafters/stable")
 
     def source(self):
         file_ext = ".tar.gz" if not self.settings.os == "Windows" else "-win32-src.zip"
         tools.get("http://archive.apache.org/dist/apr/apr-util-{v}{ext}".format(v=self.version, ext=file_ext))
 
-    def patch(self):
-        tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),
+    def _patch(self):
+        tools.replace_in_file(os.path.join(self._lib_name, 'CMakeLists.txt'),
                               "PROJECT(APR-Util C)",
                               """
                               PROJECT(APR-Util C)
@@ -42,48 +42,53 @@ class ApacheAPRUtil(ConanFile):
                               """)
 
         # Fix a ¿bug? Maybe it has changed in FindExpat module
-        tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),
+        tools.replace_in_file(os.path.join(self._lib_name, 'CMakeLists.txt'),
                               "SET(XMLLIB_INCLUDE_DIR   ${EXPAT_INCLUDE_DIRS})",
                               "SET(XMLLIB_INCLUDE_DIR   ${EXPAT_INCLUDE_DIR})")
-        tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),
+        tools.replace_in_file(os.path.join(self._lib_name, 'CMakeLists.txt'),
                               "SET(XMLLIB_LIBRARIES     ${EXPAT_LIBRARIES})",
                               "SET(XMLLIB_LIBRARIES     ${EXPAT_LIBRARY})")
 
         if self.settings.os == "Windows":
             if self.settings.build_type == "Debug":
-                tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),
+                tools.replace_in_file(os.path.join(self._lib_name, 'CMakeLists.txt'),
                                       "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/",
                                       "SET(install_bin_pdb ${install_bin_pdb} ${PROJECT_BINARY_DIR}/bin/")
-                tools.replace_in_file(os.path.join(self.lib_name, 'CMakeLists.txt'),  # TODO: Do not make it optional, grab the files and copy them.
+                tools.replace_in_file(os.path.join(self._lib_name, 'CMakeLists.txt'),  # TODO: Do not make it optional, grab the files and copy them.
                                       "          CONFIGURATIONS RelWithDebInfo Debug)",
                                       "          CONFIGURATIONS RelWithDebInfo Debug OPTIONAL)")
 
+    def _cmake_configure(self):
+        cmake = CMake(self)
+        cmake.definitions["APR_INCLUDE_DIR"] = self.deps_cpp_info["apache-apr"].include_paths[0]
+        cmake.definitions["APR_LIBRARIES"] = os.path.join(self.deps_cpp_info["apache-apr"].lib_paths[0], "libapr-1.lib")
+        cmake.configure(source_folder=self._lib_name)
+        return cmake
+
+    def _autotools_configure(self):
+        env_build = AutoToolsBuildEnvironment(self)
+        args = ['--with-apr={}'.format(self.deps_cpp_info["apache-apr"].rootpath)]
+        env_build.configure(configure_dir=self._lib_name, args=args,)
+
     def build(self):
-        self.patch()
+        self._patch()
         if self.settings.os == "Windows":
-            cmake = CMake(self)
-            cmake.definitions["APR_INCLUDE_DIR"] = self.deps_cpp_info["apache-apr"].include_paths[0]
-            cmake.definitions["APR_LIBRARIES"] = os.path.join(self.deps_cpp_info["apache-apr"].lib_paths[0], "libapr-1.lib")
-            cmake.configure(source_folder=self.lib_name)
+            cmake = self._cmake_configure()
             cmake.build()
-            cmake.install()
         else:
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build.fpic = True
-            args = ['--prefix', self.package_folder,
-                    '--with-apr={}'.format(self.deps_cpp_info["apache-apr"].rootpath),
-                    ]
-            env_build.configure(configure_dir=self.lib_name,
-                                args=args,
-                                host=self.settings.arch,
-                                build=False)  # TODO: Workaround for https://github.com/conan-io/conan/issues/2552
-            env_build.make()
-            env_build.make(args=['install'])
+            atools = self._autotools_configure()
+            atools.make()
 
     def package(self):
         # TODO: Copy files from apache-apr, this project expected them side by side
         # self.copy("*.h", dst="include", src=os.path.join(self.deps_cpp_info["apache-apr"].include_paths[0]))
-        self.copy("LICENSE", src=self.lib_name)
+        self.copy("LICENSE", src=self._lib_name)
+        if self.settings.os == "Windows":
+            cmake = self._cmake_configure()
+            cmake.install()
+        else:
+            atools = self._autotools_configure()
+            atools.install()
 
     def package_id(self):
         self.info.options.shared = "Any"  # Both, shared and static are built always
@@ -101,4 +106,3 @@ class ApacheAPRUtil(ConanFile):
                 libs += ["pthread", ]
             self.cpp_info.includedirs = [os.path.join("include", "apr-1"), ]
         self.cpp_info.libs = libs
-
